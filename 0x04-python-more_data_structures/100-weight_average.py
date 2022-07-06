32044cdbe5500d3cab5f74236f39db8fa489492e
Thembinkosi Hlophe <101598332+hlophet@users.noreply.github.com>
2022-07-06 22:07:48 +0200

    updated 100-weight_average.py

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if (len(my_list) or
-          not isinstance(my_list, list)  == 0):
+    if (len(my_list) == 0 or
+          not isinstance(my_list, list)):
         return (0)
 
     avg = 0
