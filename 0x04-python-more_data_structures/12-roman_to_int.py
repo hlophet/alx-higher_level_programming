b7deb216581242bbbde500f45a8f40e2023b1f09
Thembinkosi Hlophe <101598332+hlophet@users.noreply.github.com>
2022-07-06 22:32:37 +0200

    updated 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -18,8 +18,7 @@ def roman_to_int(roman_string):
         if roman_dic.get(roman_string[i], 0) == 0:
             return (0)
 
-        if (i != (len(roman_string) - 1) and
-                roman_dic[roman_string[i]] < roman_dic[roman_string[i + 1]]):
+        if (i != (len(roman_string) - 1) and roman_dic[roman_string[i]] < roman_dic[roman_string[i + 1]]):
                 numb += roman_dic[roman_string[i]] * -1
         else:
             numb += roman_dic[roman_string[i]]
