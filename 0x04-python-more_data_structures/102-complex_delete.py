d3747f86c3d101c5813c9d5ff6ff42f80685dbc6
Thembinkosi Hlophe <101598332+hlophet@users.noreply.github.com>
2022-07-06 22:19:25 +0200

    created 102-complex_delete.py

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+
+    for key, v in sorted(a_dictionary.items()):
+        if v == value:
+            del a_dictionary[key]
+
+    return a_dictionary
